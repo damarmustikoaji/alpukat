8365cf7451d55f198bc1d7fdaf08e0da803092e3
Damar Mustiko Aji <damarresin@gmail.com>
2022-06-17 21:59:04 +0700

    Update FUNC_LIBRARY.py

--- PUBLIC_FUNCTION/FUNC_LIBRARY.py
+++ PUBLIC_FUNCTION/FUNC_LIBRARY.py
@@ -1,11 +1,11 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
 
 import platform
 
